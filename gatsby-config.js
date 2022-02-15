d3227cf1d1ecd01e84887670e262c93786512ac9
JARAIZA <araiza@un.org>
2022-02-15 08:49:12 -0500

    added formspree

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: "Personal page of Ernesto Araiza",
     locale: "en",
     title: "💻 Ernesto Araiza - Web Developer",
+    formspreeEndpoint: "https://formspree.io/f/mlezbjba"
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
