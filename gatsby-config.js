f35098cdb141c6fba7e34214316880a77acb8b8f
JARAIZA <araiza@un.org>
2022-02-15 13:47:51 -0500

    added google analytics and domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,22 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.ernestoaraiza.com`,
     description: "Personal page of Ernesto Araiza",
     locale: "en",
     title: "💻 Ernesto Araiza - Web Developer",
     formspreeEndpoint: "https://formspree.io/f/mlezbjba"
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-32392843-1",
+        // this option places the tracking script into the head of the DOM
+        head: true,
+        // other options
+      },
+    },
+    "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
