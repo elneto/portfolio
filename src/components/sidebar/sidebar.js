bd9c6585e79daf1991fff8cc16158bb629fe3d5a
elneto <ernesto.araiza@gmail.com>
2022-03-23 05:53:37 -0400

    ghosted my header to add a homepage link, added link to home from sidebar too

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -3,6 +3,7 @@ import Location from "@wkocjan/gatsby-theme-intro/src/components/sidebar/locatio
 import ProfileImage from "@wkocjan/gatsby-theme-intro/src/components/sidebar/profile-image"
 import { arrayOf, shape, ProfileType, SocialType } from "@wkocjan/gatsby-theme-intro/src/types"
 import SocialLinks from "@wkocjan/gatsby-theme-intro/src/components/social-links/social-links"
+import { Link } from "gatsby"
 
 const Sidebar = ({ profile, social }) => (
   <aside className="w-full lg:w-1/3 lg:border-r border-line lg:px-6 xl:px-12">
@@ -11,12 +12,14 @@ const Sidebar = ({ profile, social }) => (
         <h2 className="font-header font-light text-front text-2xl leading-none mb-4">
           {profile.profession}
         </h2>
-        <h1 className="font-header font-black text-front text-5xl leading-none break-words mb-6">
-          {profile.name}
-        </h1>
-        {profile.image && (
-          <ProfileImage image={profile.image} name={profile.name} />
-        )}
+        <Link to="/">
+          <h1 className="font-header font-black text-front text-5xl leading-none break-words mb-6">
+            {profile.name}
+          </h1>
+          {profile.image && (
+            <ProfileImage image={profile.image} name={profile.name} />
+          )}
+        </Link>
         <br />
         {profile.location && (
           <Location
@@ -24,14 +27,14 @@ const Sidebar = ({ profile, social }) => (
             relocation={profile.relocation}
           />
         )}
-      <div>
-        <h2 className="font-header font-semibold text-front text-2xl leading-none mt-8">
-            Blogs
-        </h2>
-        <ul className="mt-4 underline">
-          <li><a href="/blog/2022-03-16/drupal-is-dead">16 Mar - Drupal is dead</a></li>
-        </ul>
-      </div>
+        <div>
+          <h2 className="font-header font-semibold text-front text-2xl leading-none mt-8">
+              Blogs
+          </h2>
+          <ul className="mt-4 underline">
+            <li><Link to="/blog/2022-03-16/drupal-is-dead">16 Mar - Drupal is dead</Link></li>
+          </ul>
+        </div>
       </div>
       
 
